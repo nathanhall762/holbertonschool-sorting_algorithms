8e3a8798ee7ba84fc5e18629829a3af701fecf4a
EthanWakeford <ethanwakeford@gmail.com>
2022-08-16 09:30:10 -0700

    'changed selection sort to only swap after checking every value in array'

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,19 +8,27 @@
 
 void selection_sort(int *array, size_t size)
 {
-	size_t i, tmp, j;
+	size_t i, tmp, j, lowindex;
+	int low;
 
 	for (i = 0; i + 1 < size; i++)
 	{
+		low = array[i + 1];
+		lowindex = i + 1;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[i] > array[j])
+			if (low > array[j])
 			{
-				tmp = array[i];
-				array[i] = array[j];
-				array[j] = tmp;
-				print_array(array, size);
+				low = array[j];
+				lowindex = j;
 			}
+		}
+		if (array[i] > low)
+		{
+			tmp = array[i];
+			array[i] = array[lowindex];
+			array[lowindex] = tmp;
+			print_array(array, size);
 		}
 	}
 }
